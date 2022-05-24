8f897bbd04ad24c3cc7e41c85b79089ebdb69f37
talhabmohammed <79557504+talhabmohammed@users.noreply.github.com>
2022-05-24 16:19:31 -0500

    commit 3

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { MdOutlineCancel } from 'react-icons/md';
+import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
 
-import { Button } from '.';
-import { userProfileData } from '../data/dummy';
-import { useStateContext } from '../contexts/ContextProvider';
-import avatar from '../data/avatar.jpg';
+import { Button } from ".";
+import { userProfileData } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
+import avatar from "../data/avatar.jpg";
 
 const UserProfile = () => {
   const { currentColor } = useStateContext();
@@ -12,7 +12,9 @@ const UserProfile = () => {
   return (
     <div className="nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
       <div className="flex justify-between items-center">
-        <p className="font-semibold text-lg dark:text-gray-200">User Profile</p>
+        <p className="font-semibold text-lg dark:text-gray-200">
+          User Profile
+        </p>
         <Button
           icon={<MdOutlineCancel />}
           color="rgb(153, 171, 180)"
@@ -28,25 +30,42 @@ const UserProfile = () => {
           alt="user-profile"
         />
         <div>
-          <p className="font-semibold text-xl dark:text-gray-200"> Michael Roberts </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400">  Administrator   </p>
-          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> info@shop.com </p>
+          <p className="font-semibold text-xl dark:text-gray-200">
+            Talha Mohammed
+          </p>
+          <p className="text-gray-500 text-sm dark:text-gray-400">
+            Administrator
+          </p>
+          <p className="text-gray-500 text-sm font-semibold dark:text-gray-400">
+            info@shop.com
+          </p>
         </div>
       </div>
       <div>
         {userProfileData.map((item, index) => (
-          <div key={index} className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]">
+          <div
+            key={index}
+            className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]"
+          >
             <button
               type="button"
-              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+              style={{
+                color: item.iconColor,
+                backgroundColor: item.iconBg,
+              }}
               className=" text-xl rounded-lg p-3 hover:bg-light-gray"
             >
               {item.icon}
             </button>
 
             <div>
-              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400"> {item.desc} </p>
+              <p className="font-semibold dark:text-gray-200 ">
+                {item.title}
+              </p>
+              <p className="text-gray-500 text-sm dark:text-gray-400">
+                {" "}
+                {item.desc}{" "}
+              </p>
             </div>
           </div>
         ))}
@@ -61,7 +80,6 @@ const UserProfile = () => {
         />
       </div>
     </div>
-
   );
 };
 
